24d59ea17a35b5e4b11a04b6781992a3d776a164
unknown <bubbastinx@gmail.com>
2018-06-19 20:18:14 -0600

    able to get all to dashboard

--- server.js
+++ server.js
@@ -38,12 +38,13 @@ app.listen(PORT, () => {
 //ROOT
 app.get("/getAll", (req, res) => {
     db.Article.find({}).then((dbArticle) => {
-        console.log(dbArticle + "from the ROOT route")
-        res.json(dbArticle)
+        res.render('articlesDashboard', {
+            article: dbArticle,
+
+        });
     }).catch((err) => {
         console.log(err)
     })
-
 });
 //Gets  route to scrap website
 app.get("/scrape", (req, res) => {
@@ -68,13 +69,37 @@ app.get("/scrape", (req, res) => {
                 throw err
             });
         });
-        res.render('home');
+        res.render('articlesDashboard');
     });
 }); 
+
+app.post("/submit", (req, res) => {
+    console.log(req.body)
+    db.Comment.create(req.body) 
+    .then((dbComment) => {
+        return db.Article.findOneAndUpdate({}, { $push: { Comment: dbComment_id } }, {new: true});
+    })
+    .then((dbComment) => {
+        // res.json(dbComment);
+        res.send("articlesDashboard")
+    })
+    .catch((err) => {
+        rs.json(err);
+    });
+});
+
+app.get("/comment/form", (req, res) => {
+    res.render('comment');
+})
+
+app.get("delete/:id", (req, res) => {
+
+})
 
 app.get("/", (req, res) => {
     res.render('index');
 })
+
 
 
 
